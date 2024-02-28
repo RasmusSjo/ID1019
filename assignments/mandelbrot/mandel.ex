fc9a585b53b850bebd9319b14dabd90c49b72419
RasmusSjo <rasmus.sjoberg15@gmail.com>
2024-02-28 01:29:52 +0100

    Provided functions for creating a mandelbrot image

---
+++ assignments/mandelbrot/mandel.ex
@@ -0,0 +1,24 @@
+defmodule Mandel do
+
+  def mandelbrot(width, height, x, y, k, depth) do
+    trans = fn(w, h) ->
+      Cmplx.new(x + k * (w - 1), y - k * (h - 1))
+    end
+
+    rows(width, height, trans, depth, [])
+  end
+
+  def demo() do
+    small(-2.6, 1.2, 1.2)
+  end
+
+  def small(x0, y0, xn) do
+    width = 960
+    height = 540
+    depth = 64
+    k = (xn - x0) / width
+    image = Mandel.mandelbrot(width, height, x0, y0, k, depth)
+    PPM.write("output/small.png", image)
+  end
+
+end
